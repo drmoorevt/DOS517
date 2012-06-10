13b0caaa1cf0d56f9e5aa7237b200dc87a12e5bf
Cash Cycle solutions <mobile@totalbillings.com>
2012-06-10 11:53:34 -0400

    test project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestDos::Application.config.secret_token = '8f858b94dc4baf90af24ea64c9e0acb13bf17e3b3ae7170e58aa223dc05018d2e3cf798a3dcfacdf15ee518381dfa58fdd8a316256d8054e591c92663df8f691'
