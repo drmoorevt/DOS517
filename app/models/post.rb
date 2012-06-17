bb67c51543253f4824c3851391a7d2eb20dc6bd1
Cash Cycle solutions <mobile@totalbillings.com>
2012-06-17 19:30:24 -0400

    post model constraints for title, content, user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title, :user_id
   belongs_to :user
-  has_many :comments
+  #delete related comments if we delete this post
+  has_many :comments  , :dependent => :destroy
+
+  validates :content,  :presence => true, :length => { :maximum => 140 }
+  validates :title,  :presence => true ,:length => { :maximum => 50 }
+  validates :user_id,  :presence => true
+
 end
