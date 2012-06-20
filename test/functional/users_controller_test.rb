2576ecb3552bdaa9cbbe4de78761087b00931f16
Cash Cycle solutions <mobile@totalbillings.com>
2012-06-19 23:44:50 -0400

    session populate for users controller test

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
+require 'users_controller'
+
+#reraise any exception that was caught during testing
+class UsersController; def rescue_action(e) raise e end; end
 
 class UsersControllerTest < ActionController::TestCase
   setup do
+    @controller = UsersController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
     @user = users(:one)
   end
 
@@ -17,19 +24,24 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should create user" do
+   p @user
     assert_difference('User.count') do
-      post :create, user: { admin_flag: @user.admin_flag, description: @user.description, email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password: @user.password, username: @user.username }
+      post :create, user: { admin_flag: @user.admin_flag, description: @user.description, email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password: @user.password,password_confirmation: @user.password, username: @user.username }
     end
 
     assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
+  test "should redirect user for show action if not logged" do
+    get :show, id: @user.id
+    assert_response :redirect
+    assert_redirected_to :action => "login" ,:controller => "auth"
+    assert_equal 'Please log in to access this page', flash[:notice]
   end
 
   test "should get edit" do
+    #fix login
+    session[:user_id] = @user.id
     get :edit, id: @user
     assert_response :success
   end
