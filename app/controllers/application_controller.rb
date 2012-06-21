fcd10c33bd3b5d8360e8850051d262ce26e1c065
Cash Cycle solutions <mobile@totalbillings.com>
2012-06-21 01:28:14 -0400

    search will return posts for username. update to sql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,9 @@ class ApplicationController < ActionController::Base
   #find posts by username or partial content and title match
   def get_posts(searchString)
     if searchString
-      @posts = Post.find_by_sql("select *,(select count(id) from comments where post_id = posts.id) as vote from posts
+      @posts = Post.find_by_sql("select *,(select count(id) from comments where post_id = p.id) as vote from posts p
        where content like '%#{searchString}%' or title like '%#{searchString}%'
-       or exists (select username from users where username = '#{searchString}' )
+       or exists (select username from users where username = '#{searchString}' and id = p.user_id  )
        order by vote desc, created_at desc")
     else
       @posts = Post.find_by_sql("select top 10 *,(select count(id) from comments where post_id = posts.id) as vote from posts
